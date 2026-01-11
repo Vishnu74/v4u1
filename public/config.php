bf3f72597daef953f26deb2c3896c63220c0e77d
Bharath <bharathmca001@gmail.com>
2026-01-11 18:33:01 +0530

    commit

--- public/config.php
+++ public/config.php
@@ -2,9 +2,9 @@
 date_default_timezone_set('Asia/Kolkata');
 
 /* ================= GRAPH CONFIG ================= */
-define('TENANT_ID', 'f8009090-b4dc-400b-ae14-c7590a48952d');
-define('CLIENT_ID', '437c9f06-d8d5-48e4-826c-a1ed38c8b8ca');
-define('CLIENT_SECRET', '5aP8Q~Znl5lzGjqCIetb6nwxc8Hz2jvG-OmGHcBM');
+define('TENANT_ID', '');
+define('CLIENT_ID', '');
+define('CLIENT_SECRET', '');
 
 define('FROM_EMAIL', 'info@v4usteeldetailing.com');
 define('ADMIN_EMAIL', 'selvakumar@v4usteeldetailing.com');
@@ -15,5 +15,5 @@ define('AUTO_REPLY_ENABLED', false); // true / false
 /* ================= DB CONFIG ================= */
 define('DB_HOST', 'localhost');
 define('DB_USER', 'v4umaster');
-define('DB_PASS', 'Bharath#@123');
-define('DB_NAME', 'v4umaster');
+define('DB_PASS', '');
+define('DB_NAME', '');
